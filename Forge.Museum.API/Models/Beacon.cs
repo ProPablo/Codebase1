6d26da322e287aa186545fbff6a7c9de8ecda1b8
ProPablo <40000658+ProPablo@users.noreply.github.com>
2020-09-15 15:31:55 +1000

    Changing connection string on frontend

---
+++ Forge.Museum.API/Models/Beacon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Forge.Museum.API.Models
+{
+    public class Beacon : Base
+    {
+        public Beacon()
+        {
+
+        }
+    }
+}
