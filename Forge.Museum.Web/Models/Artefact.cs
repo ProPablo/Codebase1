5bda5949f029205f922ad328bf6c1d6b3ad61553
Anhad <anhadrs@gmail.com>
2020-09-22 13:05:24 +1000

    Beacon Backend changes + connection string

--- Forge.Museum.Web/Models/Artefact.cs
+++ Forge.Museum.Web/Models/Artefact.cs
@@ -1,10 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
 namespace Forge.Museum.Web.Models
 {
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
 
     public partial class Artefact
     {
--- Forge.Museum.Web/Models/Beacon.cs
+++ Forge.Museum.Web/Models/Beacon.cs
@@ -5,16 +5,19 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace Forge.Museum.API.Models
+namespace Forge.Museum.Web.Models
 {
     public partial class Beacon
     {
         public int Id { get; set; }
 
         [Required]
+        public int Visits { get; set; }
         public string Name { get; set; }
-
+        public double Coord_X { get; set; }
+        public double Coord_Y { get; set; }
         public string Description { get; set; }
+        public string MAC_address { get; set; }
 
         public virtual ICollection<Artefact> Artefacts { get; set; }
     }
